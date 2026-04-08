bf468b169f2b3dab5646a82f4ed434fb04d3f4a4
Divyansh123ya <rajpootdivyansh344@gmail.com>
2026-04-08 16:13:54 +0530

    Update profile.py

--- views/profile.py
+++ views/profile.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 st.header('Profile')
 col1,col2 = st.columns([1,3])
 with col1:
@@ -6,3 +7,12 @@ with col1:
 with col2:
     st.write('Name: Divyansh Rajpoot')
     st.write('Email: divyansh123ya@gmail.com')
+data = {
+    "Level": ["10th", "12th", "Graduation 1st year","Graduation 2nd year"],
+    "Board/University": ["CBSE", "CBSE", "ABES Engineering College","ABES Engineering College"],
+    "Year": ["2021", "2023", "2024","2025"],
+    "CGPA": ["80.66%", "84.2%", "7.95","8.0"]
+}
+
+df = pd.DataFrame(data)
+st.table(df)
